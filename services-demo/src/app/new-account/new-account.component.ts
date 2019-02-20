621a269b43398914aabf55dd8efef58d6c1d518f
MJunaidK <junaidcts@gmail.com>
2019-02-19 19:33:50 -0500

    Logging Service

--- services-demo/src/app/new-account/new-account.component.ts
+++ services-demo/src/app/new-account/new-account.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { LoggingService} from '../logging.service';
 
 @Component({
   selector: 'app-new-account',
@@ -15,6 +16,9 @@ export class NewAccountComponent implements OnInit {
 
   onCreateAccount(accountName: string, accountStatus: string){
     this.accountAdded.emit({name: accountName,status: accountStatus});
+    //Incorrect way to instantiate a service
+     const service = new LoggingService();
+     service.logStatusChange('A server status changed, new status: '+ accountStatus);
   }
 
 }
