815a74450c257f98eb639537b1eae11a754a0a4d
MJunaidK <junaidcts@gmail.com>
2019-02-19 22:20:05 -0500

    Injecting Services into Services

--- services-demo/src/app/account/account.component.ts
+++ services-demo/src/app/account/account.component.ts
@@ -7,7 +7,7 @@ import { AccountsService } from '../accounts.service';
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.css'],
-  providers: [LoggingService]
+  //providers: [LoggingService]
 })
 export class AccountComponent implements OnInit {
 
@@ -23,7 +23,7 @@ export class AccountComponent implements OnInit {
 
   onSetTo(status: string){
     this.accountsService.updateAccount(this.id,status);
-    this.loggingService.logStatusChange('Status updated to ' + status);
+  //  this.loggingService.logStatusChange('Status updated to ' + status);
   }
 
 }
