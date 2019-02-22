c53f63e85724777d10623d8211a7e512528c05dc
MJunaidK <junaidcts@gmail.com>
2019-02-21 22:26:48 -0500

    Adding Ingredients to Recepies

--- course-project/src/app/recipes/recipe.service.ts
+++ course-project/src/app/recipes/recipe.service.ts
@@ -1,12 +1,20 @@
 import {Recipe} from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService{
 
   recipeSelected = new EventEmitter<Recipe>();
 
  private recipes: Recipe[]=[
-        new Recipe('A Test Recipe','This is simply a test','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwqzwLuQXuQug8Tqvc7jrzHVFGba-V1bm4gtWFPjMyE_Ufo2OR')
+        new Recipe(
+          'Chilly Chicken',
+          'Indians Favourite Dish',
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwqzwLuQXuQug8Tqvc7jrzHVFGba-V1bm4gtWFPjMyE_Ufo2OR',
+          [new Ingredient('Meat', 1),
+           new Ingredient('Masalas', 10) 
+        ])
+          
   ];
 
   getRecipes(){
