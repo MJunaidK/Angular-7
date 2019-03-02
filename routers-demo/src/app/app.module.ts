06e191ad4aad1802f605d58a659f3d0e58e6187f
MJunaidK <junaidcts@gmail.com>
2019-03-02 16:29:24 -0500

    Passing static data to a route

--- routers-demo/src/app/app.module.ts
+++ routers-demo/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { AuthService } from './auth.service';
 import { AuthGuard } from './auth-guard.service';
 import { CanDeactivateGuard } from './servers/edit-server/can-deactive-guard.service';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 
 @NgModule({
@@ -25,7 +26,8 @@ import { CanDeactivateGuard } from './servers/edit-server/can-deactive-guard.ser
     EditServerComponent,
     ServerComponent,
     UserComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule,
