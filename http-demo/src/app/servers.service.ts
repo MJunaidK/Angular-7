7f06765292e0133d62cf39db07a92e99ca8d5fad
MJunaidK <junaidcts@gmail.com>
2019-03-16 19:59:08 -0400

    Sending Http Get Request

--- http-demo/src/app/servers.service.ts
+++ http-demo/src/app/servers.service.ts
@@ -10,4 +10,9 @@ export class ServersService {
         const headers = new Headers({'Content-Type': 'application/json'})
         return this.http.post('https://udemy-ng-http.firebaseio.com/data.json',servers, {headers: headers} );
     }
+
+
+    getServers(){
+        return this.http.get('https://udemy-ng-http.firebaseio.com/data.json')
+    }
 }
