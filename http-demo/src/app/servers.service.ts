197c5c5337c6b820f76a691676f4b3ee006efb51
MJunaidK <junaidcts@gmail.com>
2019-03-16 20:04:10 -0400

    Sending Http PUT request

--- http-demo/src/app/servers.service.ts
+++ http-demo/src/app/servers.service.ts
@@ -6,11 +6,16 @@ export class ServersService {
 
     constructor(private http: Http){}
 
-    storeServers(servers: any[]){
-        const headers = new Headers({'Content-Type': 'application/json'})
-        return this.http.post('https://udemy-ng-http.firebaseio.com/data.json',servers, {headers: headers} );
-    }
+    // storeServers(servers: any[]){
+    //     const headers = new Headers({'Content-Type': 'application/json'})
+    //     return this.http.post('https://udemy-ng-http.firebaseio.com/data.json',servers, {headers: headers} );
+    // }
 
+
+     storeServers(servers: any[]){
+         const headers = new Headers({'Content-Type': 'application/json'})
+         return this.http.put('https://udemy-ng-http.firebaseio.com/data.json',servers, {headers: headers} );
+     }
 
     getServers(){
         return this.http.get('https://udemy-ng-http.firebaseio.com/data.json')
