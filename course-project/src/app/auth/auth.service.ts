ef71d7e35bdabac35b5a46f5caaa52db1d78955e
MJunaidK <junaidcts@gmail.com>
2019-03-17 11:44:24 -0400

    Signing Users up

--- course-project/src/app/auth/auth.service.ts
+++ course-project/src/app/auth/auth.service.ts
@@ -1,6 +1,14 @@
+import * as firebase from 'firebase';
+import { Injectable } from '@angular/core';
+@Injectable()
 export class AuthService {
-
     signUpUser(email: string, password: string){
-
+        firebase.auth().createUserWithEmailAndPassword(email,password)
+            .then((resolve) => {
+                console.log('User Created')
+            })
+            .catch(
+                error => console.log(error)
+            )
     }
 }
