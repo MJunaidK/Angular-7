76aceb1a45e06a099bcfb89aef882e2e5852d8f5
MJunaidK <junaidcts@gmail.com>
2019-03-17 17:26:19 -0400

    Creating a shopping List Feature Module

--- course-project/src/app/app.module.ts
+++ course-project/src/app/app.module.ts
@@ -3,8 +3,6 @@ import { NgModule } from '@angular/core';
 import {HttpModule} from '@angular/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
@@ -16,6 +14,7 @@ import { AuthService } from './auth/auth.service';
 import { AuthGuard } from './auth/auth-guard.service';
 import { RecipesModule } from './recipes/recipes.module';
 import { SharedModule } from './shared/shared.module';
+import { ShoppingListModule } from './shopping-list/shopping-list.module';
 
 
 
@@ -23,8 +22,6 @@ import { SharedModule } from './shared/shared.module';
   declarations: [
     AppComponent,
     HeaderComponent,
-    ShoppingListComponent,
-    ShoppingEditComponent, 
     SignupComponent,
     SigninComponent
   ],
@@ -34,7 +31,8 @@ import { SharedModule } from './shared/shared.module';
     HttpModule,  
     SharedModule,  
     RecipesModule,
-    AppRoutingModule    
+    AppRoutingModule,
+    ShoppingListModule    
   ],
   providers: [ShoppingListService, RecipeService,DataStorageService, AuthService,AuthGuard],
   bootstrap: [AppComponent]
