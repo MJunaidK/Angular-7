d78483a7b69cd52441f5c2159b7ce5fa3b10da91
MJunaidK <junaidcts@gmail.com>
2019-03-17 18:38:27 -0400

    Creating Auth Feature Module

--- course-project/src/app/app.module.ts
+++ course-project/src/app/app.module.ts
@@ -5,34 +5,30 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
-import { FormsModule } from '@angular/forms';
 import { RecipeService } from './recipes/recipe.service';
 import { DataStorageService } from './shared/data-storage.service';
-import { SignupComponent } from './auth/signup/signup.component';
-import { SigninComponent } from './auth/signin/signin.component';
 import { AuthService } from './auth/auth.service';
 import { AuthGuard } from './auth/auth-guard.service';
 import { RecipesModule } from './recipes/recipes.module';
 import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
+import { AuthModule } from './auth/auth.module';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    SignupComponent,
-    SigninComponent
+    HeaderComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
+    BrowserModule,    
     HttpModule,  
     SharedModule,  
     RecipesModule,
     AppRoutingModule,
-    ShoppingListModule    
+    ShoppingListModule,
+    AuthModule    
   ],
   providers: [ShoppingListService, RecipeService,DataStorageService, AuthService,AuthGuard],
   bootstrap: [AppComponent]
