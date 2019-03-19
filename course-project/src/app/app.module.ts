907d2daaf950aad757774123509651874d51f8c8
MJunaidK <junaidcts@gmail.com>
2019-03-18 20:16:02 -0400

    Creating a basic core module

--- course-project/src/app/app.module.ts
+++ course-project/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpModule} from '@angular/http';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeService } from './recipes/recipe.service';
@@ -12,15 +11,13 @@ import { AuthGuard } from './auth/auth-guard.service';
 import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { AuthModule } from './auth/auth.module';
-import { HomeComponent } from './home/home.component';
+import { CoreModule } from './core/core.module';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    HomeComponent
+    AppComponent    
   ],
   imports: [
     BrowserModule,    
@@ -28,7 +25,8 @@ import { HomeComponent } from './home/home.component';
     SharedModule,      
     AppRoutingModule,
     ShoppingListModule,
-    AuthModule    
+    AuthModule,
+    CoreModule    
   ],
   providers: [ShoppingListService, RecipeService,DataStorageService, AuthService,AuthGuard],
   bootstrap: [AppComponent]
