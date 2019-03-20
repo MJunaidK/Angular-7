aeb0125d219f2395a9c57f62fe3a7af7caaa62ab
MJunaidK <junaidcts@gmail.com>
2019-03-19 20:35:48 -0400

    HttpClient Setting Headers

--- course-project/src/app/shared/data-storage.service.ts
+++ course-project/src/app/shared/data-storage.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
 import {map} from 'rxjs/operators';
@@ -12,12 +12,14 @@ export class DataStorageService{
     constructor(private httpClient: HttpClient, private recipeService: RecipeService, private authService: AuthService){}
 
     storeRecipes(){
-        const token = this.authService.getToken();  
+        const token = this.authService.getToken();
+        const headers = new HttpHeaders().set('Authorization','Bearer kdgfgfeuwgfuyegf');
       return this.httpClient.put('http://ng-recipe-book.firebaseio.com/recipes.json?auth='+token, this.recipeService.getRecipes(),{
-          observe: 'events'
+          observe: 'body',
+          headers: headers
       });
     }
-
+ 
     getRecipes(){
         const token = this.authService.getToken();
         //this.httpClient.get<Recipe[]>('http://ng-recipe-book.firebaseio.com/recipes.json?auth='+token)
