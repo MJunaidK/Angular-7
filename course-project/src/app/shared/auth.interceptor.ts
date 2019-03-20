df1695a37b908f608793ee1d6f5014fb48502a3d
MJunaidK <junaidcts@gmail.com>
2019-03-19 22:08:54 -0400

    HttpClient Modifying Request

--- course-project/src/app/shared/auth.interceptor.ts
+++ course-project/src/app/shared/auth.interceptor.ts
@@ -1,10 +1,17 @@
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
 
+@Injectable()
 export class AuthInterceptor implements HttpInterceptor{
+
+    constructor(private authService: AuthService){}
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>{
         console.log('Intercepted!', req);
-        return next.handle(req);
+        //const copiedRequest = req.clone({headers: req.headers.set('','')});
+        const copiedRequest = req.clone({params: req.params.set('auth','')})
+        return next.handle(copiedRequest);
     }
 }
